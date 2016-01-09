d8911b9bd99f5e5f4ace33aab60f4033640a185c
toyknight <toyknight@outlook.com>
2016-01-09 18:34:06 +0800

    update >wolf magic defence 0 -> 5 >catapult magic defence growth 0 -> 5 >dragon attack 65 -> 70 >wolf rider price 750 -> 700, attack 75 -> 70 guardian -> poisoner >wolf archer price 800 -> 700 attack 50 -> 55 physical/magical defence +5

--- core/src/com/toyknight/aeii/utils/ClassRegister.java
+++ core/src/com/toyknight/aeii/utils/ClassRegister.java
@@ -37,12 +37,8 @@ public class ClassRegister {
         kryo.register(Notification.class);
 
         kryo.register(Array.class);
-        kryo.register(Array.ArrayIterable.class);
-        kryo.register(Array.ArrayIterator.class);
         kryo.register(ObjectSet.class);
         kryo.register(ObjectMap.class);
-        kryo.register(ObjectMap.Keys.class);
-        kryo.register(ObjectMap.Values.class);
 
         kryo.register(Point.class);
         kryo.register(Point[].class);
