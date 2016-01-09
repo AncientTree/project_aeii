a3abda6ebdccff7cc8defeecf9c879aa2f3f4660
toyknight <toyknight@outlook.com>
2016-01-09 19:14:10 +0800

    version 1.0.8c -> 1.0.8d

--- core/src/com/toyknight/aeii/GameContext.java
+++ core/src/com/toyknight/aeii/GameContext.java
@@ -34,7 +34,7 @@ public class GameContext extends Game {
 
     public static final Object RENDER_LOCK = new Object();
 
-    public static final String VERSION = "1.0.8c";
+    public static final String VERSION = "1.0.8d";
     private static final String TAG = "Main";
 
     private final int TILE_SIZE;
