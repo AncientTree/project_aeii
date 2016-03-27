875aed77ce2572f0979be140dd72fa63c87cc790
Leiping Gong <lg229@njit.edu>
2016-03-26 20:05:51 -0400

    update

--- core/src/com/toyknight/aeii/GameContext.java
+++ core/src/com/toyknight/aeii/GameContext.java
@@ -34,7 +34,7 @@ public class GameContext extends Game {
 
     public static final Object RENDER_LOCK = new Object();
 
-    public static final String VERSION = "1.0.8e-r1";
+    public static final String VERSION = "1.0.8e-r2";
     private static final String TAG = "Main";
 
     private final int TILE_SIZE;
