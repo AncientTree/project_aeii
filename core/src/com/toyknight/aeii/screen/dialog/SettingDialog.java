b09c7c115f86ba56a09400ea78a4f336e40c4a63
toyknight <toyknight@outlook.com>
2016-06-15 15:57:09 -0400

    a little code clean

--- core/src/com/toyknight/aeii/screen/dialog/SettingDialog.java
+++ core/src/com/toyknight/aeii/screen/dialog/SettingDialog.java
@@ -1,7 +1,6 @@
 package com.toyknight.aeii.screen.dialog;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
