114f3a86c63bab5c68463fe657c0c2fecfbfc818
Leiping Gong <lg229@njit.edu>
2016-06-14 22:36:56 -0400

    version -> 1.0.9c

--- core/src/com/toyknight/aeii/GameContext.java
+++ core/src/com/toyknight/aeii/GameContext.java
@@ -36,7 +36,7 @@ public class GameContext extends Game implements GameManagerListener {
 
     public static final Object RENDER_LOCK = new Object();
 
-    public static final String VERSION = "1.0.9b+";
+    public static final String VERSION = "1.0.9c";
     private static final String TAG = "Main";
 
     private final int TILE_SIZE;
