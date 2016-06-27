ed2b253c785db19fb9cf1860a6a9074696d3c93d
Leiping Gong <lg229@njit.edu>
2016-06-27 10:21:11 -0400

    update ios launcher

--- ios/src/net/toyknight/aeii/IOSLauncher.java
+++ ios/src/net/toyknight/aeii/IOSLauncher.java
@@ -6,12 +6,17 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import org.robovm.apple.uikit.UIScreen;
 
 public class IOSLauncher extends IOSApplication.Delegate {
+
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new GameContext(Platform.iOS, 48), config);
+        config.orientationPortrait = false;
+        config.orientationLandscape = true;
+        int ts = (int) (UIScreen.getMainScreen().getBounds().getHeight() / 10);
+        return new IOSApplication(new GameContext(Platform.iOS, ts), config);
     }
 
     public static void main(String[] argv) {
@@ -19,4 +24,5 @@ public class IOSLauncher extends IOSApplication.Delegate {
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
     }
+
 }
