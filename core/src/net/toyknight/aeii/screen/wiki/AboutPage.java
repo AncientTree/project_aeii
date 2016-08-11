d08e54bb9bffd820d4b0d2c44de8c62448e63fe5
toyknight <toyknight@outlook.com>
2016-08-11 11:44:51 -0400

    add Vietnamese support thanks to P. Thanh Huy

--- core/src/net/toyknight/aeii/screen/wiki/AboutPage.java
+++ core/src/net/toyknight/aeii/screen/wiki/AboutPage.java
@@ -54,7 +54,7 @@ public class AboutPage extends AEIITable {
         label_balancing.setColor(90 / 256f, 150 / 256f, 77 / 256f, 1.0f);
         add(label_balancing).width(ts * 7).padBottom(ts / 8).padTop(ts / 8).row();
 
-        addName("blackwave");
+        addName("blankwave");
         addName("youxing");
         addName("td209");
         addName("Luke");
@@ -64,7 +64,8 @@ public class AboutPage extends AEIITable {
         label_translation.setColor(90 / 256f, 150 / 256f, 77 / 256f, 1.0f);
         add(label_translation).width(ts * 7).padBottom(ts / 8).padTop(ts / 8).row();
 
-        addName("AncientTree (zh_CN)");
+        addName("AncientTree (Chinese)");
+        addName("P. Thanh Huy (Vietnamese)");
     }
 
     private void addName(String name) {
