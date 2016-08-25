3a26a31ebd1eede5830c6c1d00db33c5e0df4689
toyknight <toyknight@outlook.com>
2016-08-25 17:22:14 -0400

    Revert "version -> 1.1.9"
    This reverts commit 12c8445f907832b6880e29bb58d86f058ebaf0c5.

--- core/src/net/toyknight/aeii/GameContext.java
+++ core/src/net/toyknight/aeii/GameContext.java
@@ -36,7 +36,7 @@ public class GameContext extends Game implements GameManagerListener {
     public static final Object RENDER_LOCK = new Object();
 
     public static final String INTERNAL_VERSION = "22";
-    public static final String EXTERNAL_VERSION = "1.1.9";
+    public static final String EXTERNAL_VERSION = "1.1.9 beta";
     private static final String TAG = "Main";
 
     private final int TILE_SIZE;
