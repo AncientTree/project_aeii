35cea90df467d646cf0b538629d56e63cce62acf
toyknight <toyknight@outlook.com>
2016-09-15 21:50:22 -0400

    game version -> 1.2.1

--- core/src/net/toyknight/aeii/GameContext.java
+++ core/src/net/toyknight/aeii/GameContext.java
@@ -34,8 +34,8 @@ public class GameContext extends Game implements GameManagerListener {
 
     public static final Object RENDER_LOCK = new Object();
 
-    public static final String INTERNAL_VERSION = "23";
-    public static final String EXTERNAL_VERSION = "1.2.0";
+    public static final String INTERNAL_VERSION = "24";
+    public static final String EXTERNAL_VERSION = "1.2.1";
     private static final String TAG = "Main";
 
     private final int TILE_SIZE;
